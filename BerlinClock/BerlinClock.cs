d4c839cc032a363ba8abb05ef5b3e492d455ebf0
Bartosz Wozniak <bartosz.wozniak@hotmail.com>
2020-04-19 14:09:05 +0200

    Refactor lights creation to get rid of duplication

--- BerlinClock/BerlinClock.cs
+++ BerlinClock/BerlinClock.cs
@@ -37,48 +37,48 @@ namespace BerlinClock
 
         private IList<Light> MakeFirstRow(Time time)
         {
-            var hours = time.Hour;
-            var countOn = hours / 5;
-            var lightsOn = CreateLights(Light.On, countOn);
-            var lightsOff = CreateLights(Light.Off, 4 - countOn);
-
-            return lightsOn.Concat(lightsOff).ToList();
+            return MakeMajorTickLights(time.Hour, 5, 4);
         }
 
         private IList<Light> MakeSecondRow(Time time)
         {
-            var hours = time.Hour;
-            var countOn = hours % 5;
-            var lightsOn = CreateLights(Light.On, countOn);
-            var lightsOff = CreateLights(Light.Off, 4 - countOn);
-
-            return lightsOn.Concat(lightsOff).ToList();
+            return MakeMinorTickLights(time.Hour, 5, 4);
         }
 
         private IList<Light> MakeThirdRow(Time time)
         {
-            var minutes = time.Minute;
-            var countOn = minutes / 5;
-            var lightsOn = CreateLights(Light.On, countOn);
-            var lightsOff = CreateLights(Light.Off, 11 - countOn);
-
-            return lightsOn.Concat(lightsOff).ToList();
+            return MakeMajorTickLights(time.Minute, 5, 11);
         }
 
         private IList<Light> MakeFourthRow(Time time)
         {
-            var minutes = time.Minute;
-            var countOn = minutes % 5;
-            var lightsOn = CreateLights(Light.On, countOn);
-            var lightsOff = CreateLights(Light.Off, 4 - countOn);
-
-            return lightsOn.Concat(lightsOff).ToList();
+            return MakeMinorTickLights(time.Minute, 5, 4);
         }
 
-        private static IEnumerable<Light> CreateLights(Light element, int count)
+        private IList<Light> MakeMajorTickLights(int value, int tickValue, int lightsCount)
         {
-            for (int i = 0; i < count; ++i)
-                yield return element;
+            var countOn = value / tickValue;
+            return MakeLights(lightsCount, countOn);
+        }
+
+        private IList<Light> MakeMinorTickLights(int value, int tickValue, int lightsCount)
+        {
+            var countOn = value % tickValue;
+            return MakeLights(lightsCount, countOn);
+        }
+
+        private static IList<Light> MakeLights(int lightsCount, int countOn)
+        {
+            var lightsOn = CreateLights(Light.On, countOn);
+            var lightsOff = CreateLights(Light.Off, lightsCount - countOn);
+
+            return lightsOn.Concat(lightsOff).ToList();
+
+            IEnumerable<Light> CreateLights(Light element, int count)
+            {
+                for (int i = 0; i < count; ++i)
+                    yield return element;
+            }
         }
     }
 }
