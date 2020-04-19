63e8df0fa191ca077a2b3a7a4066676378ad688c
Bartosz Wozniak <bartosz.wozniak@hotmail.com>
2020-04-19 18:36:31 +0200

    Assert errors are thrown from parser

--- BerlinClock.UnitTests/SimpleTimeParserTests.cs
+++ BerlinClock.UnitTests/SimpleTimeParserTests.cs
@@ -4,19 +4,37 @@ using NUnit.Framework;
 namespace BerlinClock.UnitTests
 {
     [TestFixture]
-    public class TimeParserTests
+    public class SimpleTimeParserTests
     {
         [TestCase("00:00:00", 0, 0, 0)]
         [TestCase("01:01:01", 1, 1, 1)]
         [TestCase("10:10:10", 10, 10, 10)]
+        [TestCase("24:00:00", 24, 0, 0)]
         public void WhenParsingTimeStringsComponentsCanBeSeparatedByColon(string testString, int expectedHour, int expectedMinute, int expectedSecond)
         {
-            var parser = new TimeParser();
+            var parser = new SimpleTimeParser();
             var time = parser.Parse(testString);
 
             Assert.That(time.Hour, Is.EqualTo(expectedHour));
             Assert.That(time.Minute, Is.EqualTo(expectedMinute));
             Assert.That(time.Second, Is.EqualTo(expectedSecond));
+        }
+
+        [Test]
+        public void WhenParsingTimeStringsWithoutAllComponentsParserThrows()
+        {
+            var testString = "11:22";
+            var parser = new SimpleTimeParser();
+
+            Assert.Throws<FormatException>(() => parser.Parse(testString));
+        }
+
+        [Test]
+        public void WhenParsingTimeArgumentMustBeGiven()
+        {
+            var parser = new SimpleTimeParser();
+
+            Assert.Throws<ArgumentException>(() => parser.Parse(null));
         }
     }
 }
---
+++ BerlinClock/SimpleTimeParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BerlinClock;
+
+public class SimpleTimeParser : ITimeParser
+{
+    public Time Parse(string aTime)
+    {
+        if (aTime == null)
+            throw new ArgumentException("Argument must not be null.", nameof(aTime));
+
+        var components = aTime.Split(':');
+        if (components.Length != 3)
+            throw new FormatException($"Argument in incorrect format. Expected format is hh:mm:ss. Given: {aTime}.");
+
+        var hour = int.Parse(components[0]);
+        var minute = int.Parse(components[1]);
+        var second = int.Parse(components[2]);
+
+        return new Time(hour, minute, second);
+    }
+}
